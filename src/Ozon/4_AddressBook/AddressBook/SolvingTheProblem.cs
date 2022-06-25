a75567c380effd67d9ec1bd21738659b459de2e0
Nikolay Gorbunov <kolyan.kot.1996@mail.ru>
2022-06-25 16:27:19 +0300

    Разобрана задача с адресной книгой

---
+++ src/Ozon/4_AddressBook/AddressBook/SolvingTheProblem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+    public static class SolvingTheProblem
+    {
+        public static Dictionary<string, Queue<string>> CallNumbers(string[,] callNumbers)
+        {
+            var _callNumbers = new Dictionary<string, Queue<string>>();
+
+            for (int i = 0; i < callNumbers.GetLength(0); i++)
+            {
+                if (!_callNumbers.ContainsKey(callNumbers[i, 0])) //если имя в словаре уже есть.
+                {
+                    _callNumbers.Add(callNumbers[i, 0], new Queue<string>(5));
+                }
+
+                if(_callNumbers[callNumbers[i, 0]].Count < 5)
+                {
+                    _callNumbers[callNumbers[i, 0]].Enqueue(callNumbers[i, 1]);
+                } 
+            }
+
+            return _callNumbers;
+        }
+    }
+}
---
+++ src/Ozon/4_AddressBook/AddressBookTests/SolvingTheProblemTests.cs
@@ -0,0 +1,45 @@
+using AddressBook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AddressBookTests
+{
+    public class SolvingTheProblemTests
+    {
+        [Fact]
+        public void CallNumbers()
+        {
+            var numbers = new string[,]
+            {
+                { "test1", "12341" }, //свежая
+                { "test2", "35672" },
+                { "test3", "25673" },
+                { "test1", "12344" },
+                { "test2", "35675" },
+                { "test3", "25676" },
+                { "test1", "12347" },
+                { "test2", "35678" },
+                { "test3", "25679" },
+                { "test1", "12344" },
+                { "test2", "35672" },
+                { "test3", "25673" },
+                { "test1", "12348" },
+                { "test2", "35675" },
+                { "test3", "25676" },
+                { "test1", "12343" },
+                { "test2", "35678" },
+                { "test3", "25679" }, //старая
+            };
+
+            var callNumbers = SolvingTheProblem.CallNumbers(numbers);
+
+            var numbersTest1 = new string[] { "12341", "12344", "12347", "12344", "12348" };
+            var numbersQueueTest1 = new Queue<string>(numbersTest1);
+
+            var result = callNumbers.First(x => x.Key == "test1").Value;
+            Assert.Equal(numbersQueueTest1, result);
+        }
+    }
+}
