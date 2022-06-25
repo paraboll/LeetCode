28e03b1dbe35c101113c76c3acdb5383d8e60909
Nikolay Gorbunov <kolyan.kot.1996@mail.ru>
2022-06-25 14:40:27 +0300

    Разобрал задачу с регулярками.

---
+++ src/Ozon/3_RegistrationSubsystem/RegistrationSubsystem/SolvingTheProblem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RegistrationSubsystem
+{
+    public static class SolvingTheProblem
+    {
+        public static string[] LoginValidation(string[] logins)
+        {
+            var validLogins = new List<string>();
+            for(int i = 0;i < logins.Length; i++)
+            {
+                //Логин — это последовательность из латинских букв в любом регистре, цифр и символов «_» или «-» (подчеркивание и дефис).
+                //Логин не должен начинаться с дефиса.
+                //Логин должен иметь длину от 2 до 24 символов.
+                //Логины, которые отличаются только регистром, считаются одинаковыми.
+                var regex = new Regex(@"[0-9a-zA-Z_][0-9a-zA-Z_-]{1,23}");
+                MatchCollection matches = regex.Matches(logins[i]);
+                if(matches.Count > 0 && !validLogins.Any( x => x.ToLower() == matches.First().Value.ToLower() ))
+                {
+                    validLogins.Add(logins[i]);
+                }
+            }
+
+            return validLogins.ToArray();
+        }
+    }
+}
---
+++ src/Ozon/3_RegistrationSubsystem/RegistrationSubsystemTests/SolvingTheProblemTests.cs
@@ -0,0 +1,20 @@
+using RegistrationSubsystem;
+using System;
+using Xunit;
+
+namespace RegistrationSubsystemTests
+{
+    public class SolvingTheProblemTests
+    {
+        [Fact]
+        public void LoginValidation()
+        {
+            var logins = new string[] { "Login1", "_login", "LogiN1", "Lo_gin", "l", "l1", "-l1", "lo-gin"};
+
+            var testValidLogins = SolvingTheProblem.LoginValidation(logins);
+
+            var validLogins = new string[] { "Login1", "_login", "Lo_gin", "l1", "lo-gin"};
+            Assert.Equal(validLogins, testValidLogins);
+        }
+    }
+}
